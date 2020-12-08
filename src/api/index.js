dfdd82141f1d22ef72b8c0e08554ddd197aa29c6
thracian <anil_aydinn@outlook.com>
2020-12-08 19:01:43 +0300

    New Commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getRepos = async () => {
-  const resp = await axios.get("http://api.github.com/users/anilaydinn/repos");
+  const resp = await axios.get("https://api.github.com/users/anilaydinn/repos");
   return resp.status === 200 ? resp.data : null;
 };
